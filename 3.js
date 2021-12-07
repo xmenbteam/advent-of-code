1fd9e74c34b3c9a789041dd501a5c423637bb35c
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2021-12-07 09:45:40 +0000

    day 3/4

---
+++ 3.js
@@ -0,0 +1,22 @@
+exports.binaryDiagnostic = (data) => {
+  if (!data) return 0;
+  const refObj = {
+    1: 1,
+    0: -1,
+  };
+  //   let count = 0;
+
+  let gamma = "";
+  let epsilon = "";
+
+  for (let i = 0; i < data.length; i++) {
+    let count = 0;
+    let str = data[i];
+    for (let j of str) {
+      count += refObj[data[i][j]];
+    }
+    count > 0 ? (gamma += "1") : (gamma += "0");
+    console.log(gamma);
+  }
+  console.log(gamma);
+};
