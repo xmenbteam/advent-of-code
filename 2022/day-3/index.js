ff008765d31f16ac0a991e5609f0ab10a17ccba9
bbcsample <115542207+bbcsample@users.noreply.github.com>
2022-12-03 17:51:06 +0000

    reduces

--- 2022/day-3/index.js
+++ 2022/day-3/index.js
@@ -1,16 +1,10 @@
 const letterValueFinder = (string) => {
-  let letterValue = 0
-  if (!string) return letterValue
-  if (string.charCodeAt(0) >= 97 && string.charCodeAt(0) <= 122) {
-    const lowercaseValue = string.charCodeAt(0) - 96
-    letterValue += lowercaseValue
-  }
-  if (string.charCodeAt(0) >= 65 && string.charCodeAt(0) <= 90) {
-    const uppercaseValue = string.charCodeAt(0) - 38
-    letterValue += uppercaseValue
-  }
+  if (!string) return 0
+  if (string.charCodeAt(0) >= 97 && string.charCodeAt(0) <= 122)
+    return string.charCodeAt(0) - 96
 
-  return letterValue
+  if (string.charCodeAt(0) >= 65 && string.charCodeAt(0) <= 90)
+    return string.charCodeAt(0) - 38
 }
 
 const duplicateFinder = (string1, string2) => {
@@ -35,21 +29,19 @@ const triplicateFinder = (array) => {
 }
 
 const day3 = (input) => {
-  let total = 0
-
-  input.forEach((string) => {
+  return input.reduce((total, string) => {
     const firstHalf = string.slice(0, string.length / 2)
     const secondHalf = string.slice(string.length / 2)
 
     const letterInCommon = duplicateFinder(firstHalf, secondHalf)
     total += letterValueFinder(letterInCommon)
-  })
-
-  return total
+    return total
+  }, 0)
 }
 
 const day3pt2 = (array) => {
   const triplicated = threeWaySplitter(array)
+
   return triplicated.reduce((sum, curr) => {
     const letter = triplicateFinder(curr)
     const value = letterValueFinder(letter)
